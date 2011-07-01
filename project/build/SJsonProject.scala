8fbdc44db513972dbf26a979b36ffccd6b9f9d0c
Debasish Ghosh <debasish@debasishg.(none)>
2011-07-02 01:33:38 +0530

    now compiles 2.9.0.1 and using ScalaTest 1.6.1

--- project/build/SJsonProject.scala
+++ project/build/SJsonProject.scala
@@ -4,11 +4,11 @@ class SJsonProject(info: ProjectInfo) extends DefaultProject(info) with Template
 {
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
   val scalaToolsReleases = "Scala-Tools Maven2 Releases Repository" at "http://scala-tools.org/repo-releases"
-  val dispatch_json = "net.databinder" % "dispatch-json_2.9.0" % "0.8.1" % "compile"
+  val dispatch_json = "net.databinder" % "dispatch-json_2.9.0-1" % "0.8.3" % "compile"
   val commons_io = "commons-io" % "commons-io" % "1.4" % "compile"
   val objenesis = "org.objenesis" % "objenesis" % "1.2" % "compile"
   val junit = "junit" % "junit" % "4.8.1" % "test"
-  val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.4.1" % "test"
+  val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.6.1" % "test"
 
   override def packageSrcJar = defaultJarPath("-sources.jar")
   lazy val sourceArtifact = Artifact.sources(artifactID)
@@ -19,3 +19,4 @@ class SJsonProject(info: ProjectInfo) extends DefaultProject(info) with Template
   lazy val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
 //  lazy val publishTo = Resolver.file("Local Test Repository", Path fileProperty "java.io.tmpdir" asFile)
 }
+

a6ec6cd9345c3305589b56c9dda9c7a91c2b9d93
Indrajit Raychaudhuri <irc@indrajit.com>
2012-02-05 16:53:10 +0530

    Fix build config with fmppTask

--- project/SJsonProject.scala
+++ project/SJsonProject.scala
@@ -13,13 +13,12 @@ object SJsonProject extends Build
 
   lazy val coreSettings = commonSettings ++ template ++ Seq(
     name := "sjson",
-    libraryDependencies ++= Seq("net.databinder" % "dispatch-json_2.9.1" % "0.8.5" % "compile",
-                                "commons-io" % "commons-io" % "1.4" % "compile",
-                                "org.objenesis" % "objenesis" % "1.2" % "compile",
+    libraryDependencies ++= Seq("net.databinder" % "dispatch-json_2.9.1" % "0.8.5",
+                                "commons-io" % "commons-io" % "1.4",
+                                "org.objenesis" % "objenesis" % "1.2",
                                 "junit" % "junit" % "4.8.1" % "test",
                                 "org.scalatest" % "scalatest_2.9.1" % "1.6.1" % "test"),
     parallelExecution in Test := false,
-    compileOrder in Compile := CompileOrder.JavaThenScala,
     publishTo := Some("Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"),
     credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
     unmanagedResources in Compile <+= baseDirectory map { _ / "LICENSE" }
@@ -38,19 +37,17 @@ object SJsonProject extends Build
   )
 
   def fmppConfig(c: Configuration): Seq[Setting[_]] = inConfig(c)(Seq(
-    sourceGenerators <+= fmpp.identity,
+    sourceGenerators <+= fmpp,
     fmpp <<= fmppTask,
-    scalaSource <<= (baseDirectory, configuration) { (base,c) => base / (Defaults.prefix(c.name) + "src") },
     mappings in packageSrc <<= (managedSources, sourceManaged) map { (srcs, base) => srcs x relativeTo(base) },
-    sources <<= managedSources.identity
+    sources <<= managedSources
   ))
 
   lazy val fmppTask =
-    (fullClasspath in fmppConfig, runner in fmpp, unmanagedSources, scalaSource, sourceManaged, fmppOptions, streams) map { (cp, r, sources, srcRoot, output, args, s) =>
+    (fullClasspath in fmppConfig, runner in fmpp, unmanagedSources, sourceDirectory, includeFilter in unmanagedSources, sourceManaged, fmppOptions, streams) map { (cp, r, sources, srcRoot, filter, output, args, s) =>
       IO.delete(output)
       val arguments = "-U" +: "all" +: "-S" +: srcRoot.getAbsolutePath +: "-O" +: output.getAbsolutePath +: (args ++ sources.getPaths)
       toError(r.run("fmpp.tools.CommandLine", cp.files, arguments, s.log))
-      (output ** "*.scala").get
+      (output ** filter).get
   }
 }
-
